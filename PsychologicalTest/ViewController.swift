c86f29a174f655cdece8238469a8f255109b9a09
Hannah Chi <cremi468@gmail.com>
2022-04-20 16:15:57 +0800

    Initial Commit

---
+++ PsychologicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PsychologicalTest
+//
+//  Created by Hsiao-Han Chi on 2022/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
